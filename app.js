b0b3068bd68b4fbdf5d6c3d99b2b90a9c5068809
areakryte <ydavidf@gmail.com>
2018-10-08 20:55:38 -0700

    Static Elements
    Functions work on static html not dynamic yet. Creating Save point here

---
+++ app.js
@@ -0,0 +1,34 @@
+$("form button").click(function(event){
+  event.preventDefault();
+
+  var input = $("#shopping-list-entry").val();
+  appendToList(input);
+});
+
+
+function appendToList(input){
+  var listItem = "<li><span class=\"shopping-item\">" + input + "</span><div class=\"shopping-item-controls\"><button class=\"shopping-item-toggle\"><span class=\"button-label\">check</span></button><button class=\"shopping-item-delete\"><span class=\"button-label\">delete</span></button></div></li>"
+  $("ul").append(listItem);
+}
+
+
+$("ul li div .shopping-item-toggle").click(function(event){
+  event.preventDefault();
+  //parentText.css("text-decoration", "line-through");
+  //console.log(parentText);
+  var li = $(this).closest("li");
+
+  if(li.find("span:first").hasClass("shopping-item__checked")){
+    var span = li.find("span:first").removeClass("shopping-item__checked");
+  }
+  else{
+    var span = li.find("span:first").addClass("shopping-item__checked");
+  }
+});
+
+$("ul li div .shopping-item-delete").click(function(event){
+  event.preventDefault();
+  //this.parent().hide();
+  var li = $(this).closest("li");
+  li.hide();
+});
